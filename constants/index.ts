2683efbdef148a3a1cfa621c1feaf14a5e9b5c43
Dheeraj Goswami <160225779+dheerajgo34@users.noreply.github.com>
2024-04-02 20:34:49 +0530

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,42 @@
+export  const sidebarLinks = [
+    {
+        label: "home",
+        route: "/",
+        imgUrl: "/icons/Home.svg",
+
+    },
+    {
+        label: "upcoming",
+        route: "/upcoming",
+        imgUrl: "/icons/upcoming.svg",
+
+    },
+    {
+        label: "Previous",
+        route: "/previous",
+        imgUrl: "/icons/previous.svg",
+
+    },
+    {
+        label: "Recordings",
+        route: "/recordings",
+        imgUrl: "/icons/Video.svg",
+
+    },
+    {
+        label: "Personal Room",
+        route: "/personal-room",
+        imgUrl: "/icons/add-personal.svg",
+
+    },
+
+
+]
+
+export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+]
